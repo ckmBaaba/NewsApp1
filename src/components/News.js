9f36e3804629c27a1878d18348209d939b92d3d3
Chandan Kumar Mandal <chandankumarm204@gmail.com>
2025-09-14 08:16:20 +0530

    Till now is Only showing the related news which we pass the props

---
+++ src/components/News.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react'
+import NewsItem from './NewsItem'
+import Spinner from './Spinner';
+import PropTypes from 'prop-types'
+
+
+export class News extends Component {
+  static defaultProps = {
+    country:'us',
+    pageSize:8,
+    category:'general'
+
+  }
+  static propTypes = {
+    country:PropTypes.string,
+    pageSize:PropTypes.number,
+    category:PropTypes.string,
+
+    
+  }
+    
+  constructor(){
+    super();
+    console.log("Hello I am");
+    this.state={
+     articles: [],
+     loading :false,
+     page:1
+
+    }
+  }
+
+ async componentDidMount(){
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2460a17e05c64eb7a2124e6116a0c2d7&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data= await fetch(url);
+    let parsedData=await data.json()
+    console.log(parsedData);
+    this.setState ({articles: parsedData.articles, totalResults:parsedData.totalResults,loading:false})
+  }
+
+  handlePrevClick=async ()=>{
+    console.log("Prev");
+
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2460a17e05c64eb7a2124e6116a0c2d7&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true});
+    let data= await fetch(url);
+    let parsedData=await data.json()
+    console.log(parsedData);
+
+    this.setState({
+      page: this.state.page-1,
+      articles: parsedData.articles,
+      loading:false
+    })
+
+
+  }
+
+  handleNextClick=async ()=>{
+    console.log("Next");
+
+    if(!(this.state.page+1> Math.ceil(this.state.totalResults/this.props.pageSize))){
+
+    this.setState({loading:true});
+
+    let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=2460a17e05c64eb7a2124e6116a0c2d7&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    let data= await fetch(url);
+    let parsedData=await data.json()
+     
+
+    this.setState({
+      page: this.state.page+1,
+      articles: parsedData.articles,
+      loading:false
+
+
+    })
+  }
+
+  }
+
+  render() {
+    return (
+      <div className= "container my -3">
+        <h1 className='text-center'>NewsMonkey - Top Hedline</h1>
+        {this.state.loading && <Spinner/>}
+        <div className='row'>
+        {!this.state.loading && this.state.articles.map((element)=>{
+            return <div className='col-md-4'  key={element.url}>
+          <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+        </div>
+
+          })}
+          
+
+        </div>
+          <div className='container d-flex justify-content-between'>
+            <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&#8592; Previous</button>
+            <button disabled={this.state.page+1    > Math.ceil(this.state.totalResults/(this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &#8594;</button>
+            </div>
+      </div>
+    )
+  }
+}
+
+export default News
+
---
+++ src/components/NewsItem.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+
+export class NewsItem extends Component {
+  
+  
+  render() {
+
+    let {title, description, imageUrl,newsUrl}=this.props;
+    return (
+      <div className='my-3'><div className="card" style={{width: '18rem'}}>
+  <img src={!imageUrl?"https://i.insider.com/68bb059532af4d666ef75570?width=1200&format=jpeg":imageUrl} className="card-img-top" alt="..."/>
+  <div className="card-body">
+    <h5 className="card-title">{title}...</h5>
+    <p className="card-text">{description}...</p>
+    <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+  </div>
+</div>
+
+      </div>
+    )
+  }
+}
+
+export default NewsItem
